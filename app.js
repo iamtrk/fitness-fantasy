0757c2a6039373a108cf1166747655180c4bce3e
Ravikumar <ravikumar.ee@gmail.com>
2013-07-27 09:05:16 +0530

    Added logging to filesystem

--- app.js
+++ app.js
@@ -8,16 +8,17 @@ var express = require('express')
   , user = require('./routes/user')
   , foodRoutes = require('./routes/foodRoutes')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , fs   = require('fs');
 
 var app = express();
-
+var logFile = fs.createWriteStream('./myLogFile.log', {flags: 'a'});
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
-app.use(express.logger('dev'));
+app.use(express.logger({stream: logFile}));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
